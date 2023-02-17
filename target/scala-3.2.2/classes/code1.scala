9a9355ccee85c3510ca5636be682d94c9d4f2cfa
Mattis Krauch <mattis.krauch@student.uni-tuebingen.de>
2023-02-17 17:49:28 +0100

    Adds .gitignore and some necessary files

---
+++ target/scala-3.2.2/classes/code1.scala
@@ -0,0 +1,8 @@
+import VersionControlSystem.VCS
+@main
+def main(): Unit = {
+  val vcs: VCS = VCS()
+
+  vcs.generateDiffForFile("/Text1.txt", "/Text2.txt")
+  vcs.generateDiffForFile("/code1.scala", "/code2.scala")
+}
---
+++ target/scala-3.2.2/classes/code2.scala
@@ -0,0 +1,9 @@
+@main
+def main(): Unit = {
+  val vcs: VCS = VCS()
+  //Addition
+
+  vcs.generateDiffForFile("/Text1.txt", "/Text2.txt")
+  vcs.generateDiffForFile("/code1.scala", "/code2.scala") // Change
+}
+// Final addition
