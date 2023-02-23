227dc8069d096f34e656e43e66f64af5dc216984
Gesa Schönbohm <schoenbohm.gesa@t-online.de>
2023-02-24 00:14:09 +0100

    missing star

--- src/main/scala/VCS.bat
+++ src/main/scala/VCS.bat
@@ -2,6 +2,6 @@
 set arg1=%1
 set arg2=%2
 set arg3=%3
-Rem scalac %~dp0\scala\main.scala %~dp0\scala\VersionControlSystem\*.scala
+start scalac %~dp0\scala\*.scala %~dp0\scala\VersionControlSystem\*.scala
 scala %~dp0\scala\main.scala %1 %2 %3
 echo reached end
