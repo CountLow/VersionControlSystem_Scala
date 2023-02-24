348b93173d95303e1a6e6fb4b351aa3ddad9fc62
Gesa Schönbohm <schoenbohm.gesa@t-online.de>
2023-02-24 12:10:14 +0100

    its call

--- src/main/scala/VCS.bat
+++ src/main/scala/VCS.bat
@@ -2,6 +2,6 @@
 set arg1=%1
 set arg2=%2
 set arg3=%3
-start scalac %~dp0\scala\*.scala %~dp0\scala\VersionControlSystem\*.scala
+call scalac %~dp0\scala\*.scala %~dp0\scala\VersionControlSystem\*.scala
 scala %~dp0\scala\main.scala %1 %2 %3
 echo reached end
